f8bb72ddd405eb1949a559e4db449d94fac333d6
Karl Pokus <karl.pokus@gmail.com>
2016-11-20 20:22:40 +0100

    add 404

--- server.js
+++ server.js
@@ -1,7 +1,3 @@
-// root: env
-// localhost:3000
-// https://hej-node-karlpokus.c9users.io
-
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
@@ -9,6 +5,7 @@ var express = require('express'),
     dataparser = require('./lib/dataparser'),
     checkUser = require('./lib/checkUser'),
     auth = require('./lib/auth'),
+    sendLink = require('./lib/sendLink'),
     sendTheBasics = function(req, res){
       res.sendFile(__dirname + '/public/index.html');
     },
@@ -23,21 +20,19 @@ app.get('/user', sendTheBasics);
 app.get('/loginToken', sendTheBasics);
 
 // Trade e-mail for loginURLwithLoginToken
-app.post('/login', dataparser, checkUser, auth.createLoginToken, function(req, res){
-  var url = '/loginToken?loginToken=' + req.loginToken; // exclude root on localhost
-  res.end(url);
-});
+app.post('/login', dataparser, checkUser, auth.createLoginToken, sendLink);
 
 // Trade loginToken for accessToken
-app.post('/accessToken', dataparser, auth.verifyLoginToken, auth.createAccessToken, function(req, res, next){
-  res.end(req.accessToken);
-});
+app.post('/accessToken', dataparser, auth.verifyLoginToken, auth.createAccessToken);
 
 // Trade accessToken for secret
 app.post('/secret', dataparser, auth.decryptAccessToken, checkUser, function(req, res){
   res.end('hi I am secret');
 });
 
+app.use(function(req, res, next){
+  res.end('404');
+});
 app.use(errorHandler);
 
 app.listen(port, function () {
